30bc763b0d2b74115408c2776e408c1d3f3f9d9d
IvoD🪓🧙‍♂️ <ivousd@gmail.com>
2026-03-08 20:16:13 +0100

    fix: move serverComponentsExternalPackages to serverExternalPackages (Next.js 15)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['youtube-transcript'],
-  },
+  serverExternalPackages: ['youtube-transcript'],
 }
 
 module.exports = nextConfig
